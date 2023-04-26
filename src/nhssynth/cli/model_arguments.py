5681f0cd2cd8269baa0d42a14084693398613a0d
HarrisonWilde <harrisondwilde@outlook.com>
2023-04-26 20:23:15 +0100

    Add model-specific arguments and groups

---
+++ src/nhssynth/cli/model_arguments.py
@@ -0,0 +1,61 @@
+"""Define arguments for each of the models."""
+import argparse
+
+from nhssynth.common.constants import *
+
+
+def add_model_specific_args(group: argparse._ArgumentGroup, name: str, overrides: bool = False) -> None:
+    """Adds arguments to an existing group according to `name`."""
+    if name == "VAE":
+        add_vae_args(group, overrides)
+
+
+def add_vae_args(group: argparse._ArgumentGroup, overrides: bool = False) -> None:
+    """Adds arguments to an existing group for the VAE model."""
+    group.add_argument(
+        "--encoder-latent-dim",
+        type=int,
+        help="the latent dimension of the encoder",
+    )
+    group.add_argument(
+        "--encoder-hidden-dim",
+        type=int,
+        help="the hidden dimension of the encoder",
+    )
+    group.add_argument(
+        "--encoder-activation",
+        type=str,
+        choices=list(ACTIVATION_FUNCTIONS.keys()),
+        help="the activation function of the encoder",
+    )
+    group.add_argument(
+        "--encoder-learning-rate",
+        type=float,
+        help="the learning rate for the encoder",
+    )
+    group.add_argument(
+        "--decoder-latent-dim",
+        type=int,
+        help="the latent dimension of the decoder",
+    )
+    group.add_argument(
+        "--decoder-hidden-dim",
+        type=int,
+        help="the hidden dimension of the decoder",
+    )
+    group.add_argument(
+        "--decoder-activation",
+        type=str,
+        choices=list(ACTIVATION_FUNCTIONS.keys()),
+        help="the activation function of the decoder",
+    )
+    group.add_argument(
+        "--decoder-learning-rate",
+        type=float,
+        help="the learning rate for the decoder",
+    )
+    group.add_argument(
+        "--shared-optimizer",
+        action="store_true",
+        help="whether to use a shared optimizer for the encoder and decoder",
+    )
--- src/nhssynth/cli/module_arguments.py
+++ src/nhssynth/cli/module_arguments.py
@@ -1,13 +1,16 @@
 """Define arguments for each of the modules' CLI sub-parsers."""
 import argparse
 
+from nhssynth.cli.model_arguments import add_model_specific_args
 from nhssynth.common.constants import *
+from nhssynth.modules.model import MODELS
 
 
-class customAction(argparse.Action):
+class AllChoicesDefault(argparse.Action):
     """
     Customized argparse action for defaulting to the full list of choices if only the flag is supplied.
 
+    Notes:
         1) If no `option_string` is supplied: set to default value (`self.default`)
         2) If `option_string` is supplied:
             a) If `values` are supplied, set to list of values
@@ -52,7 +55,7 @@ def add_dataloader_args(parser: argparse.ArgumentParser, group_title: str, overr
         "--synthesizer",
         type=str,
         default="TVAE",
-        choices=list(SDV_SYNTHESIZER_CHOICES.keys()),
+        choices=list(SDV_SYNTHESIZERS.keys()),
         help="pick a synthesizer to use (note this can also be specified in the model module, these must match)",
     )
 
@@ -65,58 +68,24 @@ def add_model_args(parser: argparse.ArgumentParser, group_title: str, overrides:
     """Adds arguments to an existing model module sub-parser instance."""
     group = parser.add_argument_group(title=group_title)
     group.add_argument(
+        "--architecture",
+        type=str,
+        nargs="+",
+        default=["VAE"],
+        choices=list(MODELS.keys()),
+        help="the model architecture(s) to train",
+    )
+    group.add_argument(
+        "--repeats",
+        type=int,
+        default=1,
+        help="how many times to repeat the training process per model architecture (<SEED> is incremented each time)",
+    )
+    group.add_argument(
         "--model-file",
         type=str,
         default="_model",
-        help="specify the filename of the model to be saved in `experiments/<EXPERIMENT_NAME>/`, defaults to `<args.real_data>_model.pt`",
-    )
-    group.add_argument(
-        "--use-gpu",
-        action="store_true",
-        help="use the GPU for training",
-    )
-    group.add_argument(
-        "--non-private-training",
-        action="store_true",
-        help="train the model in a non-private way",
-    )
-    group.add_argument(
-        "--secure-mode",
-        action="store_true",
-        default=False,
-        help="Enable Secure RNG to have trustworthy privacy guarantees. Comes at a performance cost",
-    )
-    group.add_argument(
-        "--num-epochs",
-        type=int,
-        default=100,
-        help="number of epochs to train for",
-    )
-    group.add_argument(
-        "--tracked-metrics",
-        type=str,
-        nargs="+",
-        default=TRACKED_METRIC_CHOICES,
-        help="metrics to track during training of the DPVAE model",
-        choices=TRACKED_METRIC_CHOICES,
-    )
-    group.add_argument(
-        "--latent-dim",
-        type=int,
-        default=256,
-        help="the latent dimension of the model",
-    )
-    group.add_argument(
-        "--hidden-dim",
-        type=int,
-        default=256,
-        help="the hidden dimension of the model",
-    )
-    group.add_argument(
-        "--learning-rate",
-        type=float,
-        default=1e-3,
-        help="the learning rate for the model",
+        help="specify the filename of the model to be saved in `experiments/<EXPERIMENT_NAME>/`, defaults to `<DATASET>_model.pt`",
     )
     group.add_argument(
         "--batch-size",
@@ -125,29 +94,61 @@ def add_model_args(parser: argparse.ArgumentParser, group_title: str, overrides:
         help="the batch size for the model",
     )
     group.add_argument(
+        "--num-epochs",
+        type=int,
+        default=100,
+        help="number of epochs to train for",
+    )
+    group.add_argument(
         "--patience",
         type=int,
         default=5,
         help="how many epochs the model is allowed to train for without improvement",
     )
     group.add_argument(
+        "--tracked-metrics",
+        type=str,
+        nargs="+",
+        default=TRACKED_METRICS,
+        help="metrics to track during training of the model",
+        choices=TRACKED_METRICS,
+    )
+    group.add_argument(
+        "--use-gpu",
+        action="store_true",
+        help="use the GPU for training",
+    )
+    privacy_group = parser.add_argument_group(title="model privacy options")
+    privacy_group.add_argument(
+        "--non-private",
+        action="store_true",
+        help="train the model in a non-private way",
+    )
+    privacy_group.add_argument(
         "--target-epsilon",
         type=float,
         default=1.0,
         help="the target epsilon for differential privacy",
     )
-    group.add_argument(
+    privacy_group.add_argument(
         "--target-delta",
         type=float,
-        default=None,
         help="the target delta for differential privacy, defaults to `1 / len(dataset)` if not specified",
     )
-    group.add_argument(
+    privacy_group.add_argument(
         "--max-grad-norm",
-        type=int,
-        default=10,
+        type=float,
+        default=5.0,
         help="the clipping threshold for gradients (only relevant under differential privacy)",
     )
+    privacy_group.add_argument(
+        "--secure-mode",
+        action="store_true",
+        help="Enable secure RNG via the `csprng` package to make privacy guarantees more robust, comes at a cost of performance and reproducibility",
+    )
+    for model_name in MODELS.keys():
+        model_group = parser.add_argument_group(title=f"{model_name}-specific options")
+        add_model_specific_args(model_group, model_name, overrides=overrides)
 
 
 def generate_evaluation_arg(group, name):
@@ -156,9 +157,9 @@ def generate_evaluation_arg(group, name):
         type=str,
         default=None,
         nargs="*",
-        action=customAction,
-        const=list(SDV_METRIC_CHOICES[name].keys()),
-        choices=list(SDV_METRIC_CHOICES[name].keys()),
+        action=AllChoicesDefault,
+        const=list(SDV_METRICS[name].keys()),
+        choices=list(SDV_METRICS[name].keys()),
         help=f"run the {name.lower()} evaluation",
     )
 
@@ -176,7 +177,7 @@ def add_evaluation_args(parser: argparse.ArgumentParser, group_title: str, overr
         action="store_true",
         help="run the quality evaluation",
     )
-    for name in SDV_METRIC_CHOICES:
+    for name in SDV_METRICS:
         generate_evaluation_arg(group, name)
 
 
