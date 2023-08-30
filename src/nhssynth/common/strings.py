db5c4df277944ca11994bd3d19c48403077e915e
HarrisonWilde <harrisondwilde@outlook.com>
2023-08-30 10:19:43 +0300

    Add a function to format time deltas

--- src/nhssynth/common/strings.py
+++ src/nhssynth/common/strings.py
@@ -1,4 +1,5 @@
 """String manipulation functions."""
+import datetime
 import re
 
 
@@ -19,3 +20,29 @@ def add_spaces_before_caps(string: str) -> str:
         'Hello World AGAIN'
     """
     return " ".join(re.findall(r"[a-z]?[A-Z][a-z]+|[A-Z]+(?=[A-Z][a-z]|\b)", string))
+
+
+def format_timedelta(start, finish) -> str:
+    """
+    Calculate and format the difference between two calls to `time.time()`.
+
+    Args:
+        start: The start time.
+        finish: The finish time.
+
+    Returns:
+        A string containing the time difference in a human-readable format.
+    """
+    total = datetime.timedelta(seconds=finish - start)
+    hours, remainder = divmod(total.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    if total.days > 0:
+        delta_str = f"{total.days}d {hours}h {minutes}m {seconds}s"
+    elif hours > 0:
+        delta_str = f"{hours}h {minutes}m {seconds}s"
+    elif minutes > 0:
+        delta_str = f"{minutes}m {seconds}s"
+    else:
+        delta_str = f"{seconds}s"
+    return delta_str
