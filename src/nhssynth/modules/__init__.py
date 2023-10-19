3dd49e0d53d4fd99c601c4aba81f11bd508e45ea
HarrisonWilde <harrisondwilde@outlook.com>
2023-10-19 09:07:18 +0100

    Critical fix for module imports

---
+++ src/nhssynth/modules/__init__.py
@@ -0,0 +1,6 @@
+import nhssynth.modules.dashboard as dashboard  # noqa: F401
+import nhssynth.modules.dataloader as dataloader  # noqa: F401
+import nhssynth.modules.evaluation as evaluation  # noqa: F401
+import nhssynth.modules.model as model  # noqa: F401
+import nhssynth.modules.plotting as plotting  # noqa: F401
+import nhssynth.modules.structure as structure  # noqa: F401
